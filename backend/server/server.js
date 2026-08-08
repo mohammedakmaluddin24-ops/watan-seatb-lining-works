29e8a5f3ef6a5f2d74e83ba57ae564e2a161383b
mohammedakmaluddin24-ops <mohammedakmaluddin24@ifheindia.org>
2026-08-09 01:38:08 +0530

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,7 +7,12 @@ const authRoutes = require("./routes/auth");
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
