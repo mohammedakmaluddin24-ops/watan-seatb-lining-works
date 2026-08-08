c784e75ee30df9cb1bfa292c062f817d1aeaecd9
mohammedakmaluddin24-ops <mohammedakmaluddin24@ifheindia.org>
2026-08-09 01:06:14 +0530

    Update Login.jsx

--- frontend/client/src/pages/Login.jsx
+++ frontend/client/src/pages/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
     try {
 
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+      "https://watan-seat-lining-works-backend.onrender.com/api/auth/login",
         form
       );
 
