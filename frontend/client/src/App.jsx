8c7db30a23ad513911da3a2530f485e48795fb1c
mohammedakmaluddin24-ops <mohammedakmaluddin24@ifheindia.org>
2026-09-14 15:26:57 +0530

    Update App.jsx

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -5,22 +5,17 @@ import Customers from "./pages/Customers";
 import Works from "./pages/Works";
 import Payments from "./pages/Payments";
 import Reports from "./pages/Reports";
-import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/works" element={<Works />} />
-          <Route path="/payments" element={<Payments />} />
-          <Route path="/reports" element={<Reports />} />
-        </Route>
-
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/reports" element={<Reports />} />
       </Routes>
     </BrowserRouter>
   );
