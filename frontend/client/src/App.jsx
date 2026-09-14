b0c586332f659b66d5df87bc2116c6e2987ca6fb
mohammedakmaluddin24-ops <mohammedakmaluddin24@ifheindia.org>
2026-09-14 15:24:43 +0530

    Update App.jsx

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,28 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
 import Works from "./pages/Works";
 import Payments from "./pages/Payments";
 import Reports from "./pages/Reports";
 import MainLayout from "./layouts/MainLayout";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          element={
-            <ProtectedRoute>
-              <MainLayout />
-            </ProtectedRoute>
-          }
-        >
+        <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/customers" element={<Customers />} />
